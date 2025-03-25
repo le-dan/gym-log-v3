7d0cd26f46545d8baaebfaf1f1d3d0f1f0bc3015
mineult <tle57@gmu.edu>
2025-03-24 21:20:28 -0400

    remove sample workouts

--- src/components/Dashboard/WorkoutGrid.tsx
+++ src/components/Dashboard/WorkoutGrid.tsx
@@ -1,13 +1,10 @@
 import { WorkoutInterface } from "../../util/interfaces";
 import { useEffect, useState } from "react";
-import { testWorkouts } from "../../SampleWorkouts";
 import { aggregateMuscles, fetchData } from "../../util/util";
 import { NavLink } from "react-router";
 import NavBar from "./NavBar";
 interface WorkoutGridProps {
-	setChosenWorkout: React.Dispatch<
-		React.SetStateAction<WorkoutInterface | undefined>
-	>;
+	setChosenWorkout: React.Dispatch<React.SetStateAction<WorkoutInterface | undefined>>;
 }
 
 export default function WorkoutGrid({ setChosenWorkout }: WorkoutGridProps) {
@@ -24,10 +21,8 @@ export default function WorkoutGrid({ setChosenWorkout }: WorkoutGridProps) {
 					onClick={() => handleWorkoutClick(workout)}
 					className="bg-snow-white drop-shadow-lg rounded-lg h-35 w-[340px] justify-between items-center hover:scale-105 hover:shadow-md hover:shadow-accent ease-in-out duration-300 hover:cursor-pointer p-6 flex gap-5"
 					key={index}
-					to={
-						"/dashboard/" +
-						workout.name.replace(" ", "").toLowerCase()
-					}
+					to={"/dashboard/" + workout.name.replace(" ", "").toLowerCase()
+}
 				>
 					<div className="flex-col flex shrink-0 max-w-19">
 						<span className="font-bold">{workout.name}</span>
@@ -52,11 +47,10 @@ export default function WorkoutGrid({ setChosenWorkout }: WorkoutGridProps) {
 	const [workouts, setWorkouts] = useState<WorkoutInterface[]>([]);
 	useEffect(() => {
 		if (workouts.length === 0) {
-			setWorkouts(testWorkouts);
 			fetchData<WorkoutInterface[]>("WorkoutsDB")
 				.then((res) => {
 					if (res) {
-						setWorkouts(testWorkouts.concat(res));
+						setWorkouts(res);
 					}
 				})
 				.catch((err) => {
@@ -69,15 +63,25 @@ export default function WorkoutGrid({ setChosenWorkout }: WorkoutGridProps) {
 		<>
 			<NavBar />
 			<div className="h-full w-full bg-snow-white p-10 shadow-lg rounded-lg flex flex-col gap-10 text-text min-h-0">
-				<span className="text-3xl font-bold text-primary">
-					choose a workout for today,
-				</span>
-				<div
-					className="min-h-0 h-full w-full grid grid-rows-[repeat(auto-fill,_minmax(140px,1fr))] grid-cols-[repeat(auto-fill,_minmax(330px,1fr))] gap-8 overflow-y-auto p-4"
-					style={{ scrollbarGutter: "stable both-edges" }}
-				>
-					{renderWorkouts(workouts)}
-				</div>
+				<span className="text-4xl font-bold text-primary">choose a workout for today,</span>
+				{workouts.length > 0 ? (
+					<div
+						className="min-h-0 h-full w-full grid grid-rows-[repeat(auto-fill,_minmax(140px,1fr))] grid-cols-[repeat(auto-fill,_minmax(330px,1fr))] gap-8 overflow-y-auto p-4"
+						style={{ scrollbarGutter: "stable both-edges" }}
+					>
+						{renderWorkouts(workouts)}
+					</div>
+				) : (
+					<div className="h-full w-full flex justify-center items-center flex-col text-primary/60  text-4xl gap-12">
+						There are no custom workouts yet! Bummer! :(
+						<NavLink
+							to="/workout/create"
+							className="text-xl py-3 px-5 w-fit bg-primary text-snow-white hover-css rounded-xl hover:bg-accent hover:cursor-pointer hover:scale-105 ease-in-out"
+						>
+							create workout
+						</NavLink>
+					</div>
+				)}
 			</div>
 		</>
 	);
--- src/components/Workout/ExerciseAccordion.tsx
+++ src/components/Workout/ExerciseAccordion.tsx
@@ -64,127 +64,125 @@ export default function ExerciseAccordion({ exercise, exercisesList, setExercise
 		}));
 	}
 	return (
-		<motion.div key="accordion" initial={{ opacity: 0, scale: 0 }} exit={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }}>
-			<Accordion key={exercise.name} sx={styles.accordion} className="px-5" expanded={openAccordion}>
-				<Box sx={{ display: "flex" }}>
-					<AccordionSummary
-						onClick={() => setOpenAccordion(!openAccordion)}
-						expandIcon={<ChevronDown />}
-						className="font-semibold text-primary text-2xl"
-					>
-						{exercise.name}
-					</AccordionSummary>
-					<Box sx={{ display: "flex", alignItems: "center" }}>
-						<AnimatePresence>
-							<IconButton className="h-fit" onClick={() => handleEdit()}>
-								{editMode && (
-									<motion.div
-										key="check"
-										initial={{ opacity: 0, scale: 0 }}
-										exit={{ opacity: 0, scale: 0 }}
-										animate={{ opacity: 1, scale: 1 }}
-										transition={{ duration: 0.5, type: "spring" }}
-									>
-										<Check className="text-primary" />
-									</motion.div>
-								)}
-								{!editMode && (
-									<motion.div
-										key="pencil"
-										initial={{ opacity: 0, scale: 0 }}
-										exit={{ opacity: 0, scale: 0 }}
-										animate={{ opacity: 1, scale: 1 }}
-										transition={{ duration: 0.5, type: "spring" }}
-									>
-										<Pencil className="text-primary" />
-									</motion.div>
-								)}
-							</IconButton>
-						</AnimatePresence>
-						<IconButton className="h-fit" onClick={() => handleDelete()}>
-							<Trash className="text-red-400" />
+		<Accordion key={exercise.name} sx={styles.accordion} className="px-5" expanded={openAccordion}>
+			<Box sx={{ display: "flex" }}>
+				<AccordionSummary
+					onClick={() => setOpenAccordion(!openAccordion)}
+					expandIcon={<ChevronDown />}
+					className="font-semibold text-primary text-2xl"
+				>
+					{exercise.name}
+				</AccordionSummary>
+				<Box sx={{ display: "flex", alignItems: "center" }}>
+					<AnimatePresence>
+						<IconButton className="h-fit" onClick={() => handleEdit()}>
+							{editMode && (
+								<motion.div
+									key="check"
+									initial={{ opacity: 0, scale: 0 }}
+									exit={{ opacity: 0, scale: 0 }}
+									animate={{ opacity: 1, scale: 1 }}
+									transition={{ duration: 0.5, type: "spring" }}
+								>
+									<Check className="text-primary" />
+								</motion.div>
+							)}
+							{!editMode && (
+								<motion.div
+									key="pencil"
+									initial={{ opacity: 0, scale: 0 }}
+									exit={{ opacity: 0, scale: 0 }}
+									animate={{ opacity: 1, scale: 1 }}
+									transition={{ duration: 0.5, type: "spring" }}
+								>
+									<Pencil className="text-primary" />
+								</motion.div>
+							)}
 						</IconButton>
-					</Box>
+					</AnimatePresence>
+					<IconButton className="h-fit" onClick={() => handleDelete()}>
+						<Trash className="text-red-400" />
+					</IconButton>
 				</Box>
-				<AccordionDetails sx={{ padding: 3 }} className="text-text flex flex-col gap-8">
-					<Typography component={"div"} className="flex text-xl gap-3 text-primary justify-center items-center">
-						<TextField
-							fullWidth
-							color="primary"
+			</Box>
+			<AccordionDetails sx={{ padding: 3 }} className="text-text flex flex-col gap-8">
+				<Typography component={"div"} className="flex text-xl gap-3 text-primary justify-center items-center">
+					<TextField
+						fullWidth
+						color="primary"
+						size="small"
+						name="name"
+						label="Exercise Name"
+						variant="outlined"
+						value={exerciseForm.name}
+						onChange={(e) => handleInputChange(e)}
+						disabled={!editMode}
+					/>
+				</Typography>
+				<Typography component={"div"} className="flex text-xl gap-3 text-primary justify-center items-center">
+					<TextField
+						color="primary"
+						size="small"
+						name="sets"
+						label="Sets"
+						variant="outlined"
+						value={exerciseForm.sets}
+						onChange={(e) => handleInputChange(e)}
+						disabled={!editMode}
+					/>
+					x
+					<TextField
+						color="primary"
+						size="small"
+						label="Repetitions"
+						variant="outlined"
+						name="reps"
+						value={exerciseForm.reps}
+						onChange={(e) => handleInputChange(e)}
+						disabled={!editMode}
+					/>
+				</Typography>
+				<Typography component={"div"} className="flex flex-col">
+					<FormControl>
+						<InputLabel id="muscles-worked-label" disabled={!editMode}>
+							<em>Select muscle(s) worked</em>
+						</InputLabel>
+						<Select
+							multiple
 							size="small"
-							name="name"
-							label="Exercise Name"
-							variant="outlined"
-							value={exerciseForm.name}
-							onChange={(e) => handleInputChange(e)}
+							className="w-full"
+							labelId="muscles-worked-label"
+							label="Select muscle(s) worked"
+							name="musclesWorked"
+							value={exerciseForm.musclesWorked}
 							disabled={!editMode}
-						/>
-					</Typography>
-					<Typography component={"div"} className="flex text-xl gap-3 text-primary justify-center items-center">
-						<TextField
-							color="primary"
-							size="small"
-							name="sets"
-							label="Sets"
-							variant="outlined"
-							value={exerciseForm.sets}
 							onChange={(e) => handleInputChange(e)}
-							disabled={!editMode}
-						/>
-						x
-						<TextField
-							color="primary"
-							size="small"
-							label="Repetitions"
-							variant="outlined"
-							name="reps"
-							value={exerciseForm.reps}
-							onChange={(e) => handleInputChange(e)}
-							disabled={!editMode}
-						/>
-					</Typography>
-					<Typography component={"div"} className="flex flex-col">
-						<FormControl>
-							<InputLabel id="muscles-worked-label" disabled={!editMode}>
-								<em>Select muscle(s) worked</em>
-							</InputLabel>
-							<Select
-								multiple
-								size="small"
-								className="w-full"
-								labelId="muscles-worked-label"
-								label="Muscles Worked"
-								name="musclesWorked"
-								value={exerciseForm.musclesWorked}
-								disabled={!editMode}
-								onChange={(e) => handleInputChange(e)}
-							>
-								{Object.keys(Muscle).map((muscle) => {
-									return (
-										<MenuItem key={muscle} value={muscle}>
-											{muscle}
-										</MenuItem>
-									);
-								})}
-							</Select>
-						</FormControl>
-					</Typography>
-					<Typography component={"div"} className="flex flex-col">
-						<TextField
-							fullWidth
-							className="w-23"
-							color="primary"
-							size="small"
-							name="notes"
-							label="Notes"
-							variant="outlined"
-							value={exerciseForm.notes}
-							onChange={(e) => handleInputChange(e)}
-							disabled={!editMode}
-						/>
-					</Typography>
-				</AccordionDetails>
-			</Accordion>
-		</motion.div>
+						>
+							{Object.keys(Muscle).map((muscle) => {
+								return (
+									<MenuItem key={muscle} value={muscle}>
+										{muscle}
+									</MenuItem>
+								);
+							})}
+						</Select>
+					</FormControl>
+				</Typography>
+				<Typography component={"div"} className="flex flex-col">
+					<TextField
+						fullWidth
+						className="w-23"
+						color="primary"
+						size="small"
+						name="notes"
+						label="Notes"
+						variant="outlined"
+						value={exerciseForm.notes}
+						onChange={(e) => handleInputChange(e)}
+						disabled={!editMode}
+					/>
+				</Typography>
+			</AccordionDetails>
+		</Accordion>
 	);
 }
